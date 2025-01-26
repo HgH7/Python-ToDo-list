8b273d2a6c844c758bcac60b652d986ad2dcee83
Hassan ElSayed <ahsn17462@gmail.com>
2025-01-26 20:11:27 +0200

    This is the first version

--- TO-DO-LIST.py
+++ TO-DO-LIST.py
@@ -1,20 +1,34 @@
+tasks = []
 while True:
+    print("\n")
     print("1-Enter a task\n2-View Tasks\n3-Delete Tasks\n4-Quit")
     x = int(input("pick a choise: "))
-    if x == 1 or x == 2 or x == 3 or x == 4:
+    print()
+    if x ==4:
         break
-    else:
+    if x != 1 and x !=2 and x !=3 and x !=4:
         print("\n\n\n\nplease enter a valid number")
         continue
-tasks = []
-while x == 1:
-    j = input("enter the tasks you want to do: ")
-    tasks.append(j)
-    print("when done enter (done)")
-    if j == 'done':
+    while x == 1:
+        j = input("enter the tasks you want to do: ")
+        print("when done enter (done)")
+        if j == 'done':
+            break
+        tasks.append(j)        
+    while x == 2:
+        for num,task in enumerate(tasks, start=1):
+            print(num,'-',task)
         break
-tasks.pop(-1)
-print(tasks)
+    while x == 3:
+        v = int(input("enter the number of the task you wanna delete: "))
+        tasks.pop(v-1)
+        break
+
+
+
+
+
+
 
 
 
