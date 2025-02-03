0d5ccf9b68ff7f577447844265e904937c5fc04b
Hassan Elsayed <ahsn17462@gmail.com>
2025-02-03 08:13:36 +0200

    Full working app

--- TO-DO-LIST.py
+++ TO-DO-LIST.py
@@ -1,6 +1,9 @@
 tasks = []
 while True:
     print("\n")
+    print("Welcome to my life organize app\n")
+    print("------------------\n")
+    
     print("1-Enter a task\n2-View Tasks\n3-Delete Tasks\n4-Quit")
     x = int(input("pick a choise: "))
     print()
@@ -14,19 +17,43 @@ while True:
         print("when done enter (done)")
         if j == 'done':
             break
-        tasks.append(j)        
+        tasks.append(j)
+        with open('tasks.txt','w') as file:
+            for item in tasks:
+                file.write(item+'\n')       
     while x == 2:
-        for num,task in enumerate(tasks, start=1):
+        print("------------------\n")
+        print("Your tasks are:\n")
+        with open('tasks.txt','r') as file:
+            reading = file.readlines()
+        for num,task in enumerate(reading, start=1):
             print(num,'-',task)
+        print("------------------")
         break
     while x == 3:
         if not tasks:
             print("there's no tasks to delete")
             break
-        v = int(input("enter the number of the task you wanna delete: "))
-        tasks.pop(v-1)
-        print("Task deleted")
+        try:
+            v = int(input("enter the number of the task you wanna delete: "))
+            with open('tasks.txt','r') as file:
+                reading = file.readlines()
+            if 0 < v <= len(reading):
+                tasks.pop(v-1)
+                deleted_task = reading.pop(v-1)
+                with open('tasks.txt','w') as file:
+                    file.writelines(reading)
+                print("Task deleted")
+            else:
+                print("Invalid Task number")
+        except ValueError:
+            print("Please enter a valid number.")
+        except IndexError:
+            print("Task number out of range.")
         break
+        
+    
+
         
         
 
